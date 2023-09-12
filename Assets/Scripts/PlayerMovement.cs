4e7e6ddd1f51be62db0a02a4204b909de12bcbcf
RobinOhman <robinohmantejre@gmail.com>
2023-09-12 19:15:06 +0200

    Förbättrade Dashen
    Man kan dasha vertikalt nu

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,21 +3,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private float vertical;
     private float horizontal;
-    private float speed = 8f;
-    private float jumpingPower = 16f;
+    //Basic Movement
+    [SerializeField] private float speed = 8f;
+    [SerializeField] private float jumpingPower = 16f;
     private bool isFacingRight = true;
-
+    //Dash
     private bool canDash = true;
     private bool isDashing;
-    private float dashingPower = 24f;
-    private float dashingTime = 0.2f;
-    private float dashingCooldown = 1f;
+    private bool isDashingCooldown;
+    [SerializeField] public float dashingPower = 30f;
+    [SerializeField] public float dashingTime = 0.2f;
+    [SerializeField] public float dashingCooldown = 1f;
 
+
+    [SerializeField] private TrailRenderer tr;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask Ground;
-    [SerializeField] private TrailRenderer tr;
+
+   
     private void Start()
     {
         tr.emitting = false;
@@ -29,6 +35,7 @@ public class PlayerMovement : MonoBehaviour
             return;
         }
 
+        vertical = Input.GetAxisRaw("Vertical");
         horizontal = Input.GetAxisRaw("Horizontal");
 
         if (Input.GetButtonDown("Jump") && IsGrounded())
@@ -42,21 +49,29 @@ public class PlayerMovement : MonoBehaviour
         }
 
         if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
-        {
+        {   
+
             StartCoroutine(Dash());
         }
+        if (IsGrounded() && !isDashingCooldown)
+        {
+            canDash = true;
+        }
+
 
         Flip();
-    }
 
+       
+    }
     private void FixedUpdate()
     {
         if (isDashing)
         {
             return;
         }
-
+       
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
     }
 
     private bool IsGrounded()
@@ -74,20 +89,28 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = localScale;
         }
     }
-
     private IEnumerator Dash()
     {
         canDash = false;
         isDashing = true;
+        isDashingCooldown = true;
+        canDash = false;
+        isDashing = true;
         float originalGravity = rb.gravityScale;
         rb.gravityScale = 0f;
-        rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+        rb.velocity = new Vector2(transform.localScale.x, Input.GetAxisRaw("Vertical")* 0.5f)* dashingPower;
         tr.emitting = true;
         yield return new WaitForSeconds(dashingTime);
         tr.emitting = false;
         rb.gravityScale = originalGravity;
         isDashing = false;
+        rb.velocity = new Vector2(transform.localScale.x, 0) ;
         yield return new WaitForSeconds(dashingCooldown);
         canDash = true;
+        isDashingCooldown = false;
+
     }
+   
+
+
 }
