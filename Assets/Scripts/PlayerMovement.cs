2093118bc625badd69506a487d43785604cc1adf
RobinOhman <robinohmantejre@gmail.com>
2023-09-14 14:41:00 +0200

    Movement Fixes
    Ändrade fysiken för jumps och fixade en bug med dashen

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,32 +1,42 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     private float vertical;
     private float horizontal;
-    //Basic Movement
+    [Header("Movement")]
     [SerializeField] private float speed = 8f;
-    [SerializeField] private float jumpingPower = 16f;
     private bool isFacingRight = true;
-    //Dash
+
+    [Header("Jump")]
+    [SerializeField] private float jumpForce = 8f;
+    [SerializeField] private float jumpReleaseModifier = 2f;
+    [SerializeField] private GameObject jumpParticles;
+
+    private bool isJumping;
+    
+
+    [Header("Dash")]
+    [SerializeField] public float dashingForce = 8f;
+    [SerializeField] public float dashingTime = 0.2f;
+    [SerializeField] public float dashingCooldown = 1f;
+    [SerializeField] private TrailRenderer dashTrailRenderer;
+
     private bool canDash = true;
     private bool isDashing;
     private bool isDashingCooldown;
-    [SerializeField] public float dashingPower = 30f;
-    [SerializeField] public float dashingTime = 0.2f;
-    [SerializeField] public float dashingCooldown = 1f;
 
-
-    [SerializeField] private TrailRenderer tr;
+    [Header("Physics")]
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask Ground;
-
+    
 
     private void Start()
     {
-        tr.emitting = false;
+        dashTrailRenderer.emitting = false;
     }
     private void Update()
     {
@@ -38,14 +48,21 @@ public class PlayerMovement : MonoBehaviour
         vertical = Input.GetAxisRaw("Vertical");
         horizontal = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+        var jumpInput = Input.GetButtonDown("Jump");
+        var jumpInputRelease = Input.GetButtonUp("Jump");
+
+        if (jumpInput && IsGrounded())
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            Instantiate(jumpParticles, transform.position, jumpParticles.transform.localRotation);
+        }
+        if (jumpInputRelease && rb.velocity.y > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y / jumpReleaseModifier);
         }
 
         if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
         {
-
             StartCoroutine(Dash());
         }
         if (IsGrounded() && !isDashingCooldown)
@@ -58,6 +75,10 @@ public class PlayerMovement : MonoBehaviour
 
 
     }
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, Ground);
+    }
     private void FixedUpdate()
     {
         if (isDashing)
@@ -68,12 +89,6 @@ public class PlayerMovement : MonoBehaviour
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
 
     }
-
-    private bool IsGrounded()
-    {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, Ground);
-    }
-
     private void Flip()
     {
         if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
@@ -84,6 +99,7 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = localScale;
         }
     }
+
     private IEnumerator Dash()
     {
         canDash = false;
@@ -93,15 +109,14 @@ public class PlayerMovement : MonoBehaviour
         isDashing = true;
         float originalGravity = rb.gravityScale;
         rb.gravityScale = 0f;
-        rb.velocity = new Vector2(transform.localScale.x, Input.GetAxisRaw("Vertical")) * dashingPower;
-        tr.emitting = true;
+        rb.velocity = new Vector2(transform.localScale.x, Input.GetAxisRaw("Vertical")) * dashingForce;
+        dashTrailRenderer.emitting = true;
         yield return new WaitForSeconds(dashingTime);
-        tr.emitting = false;
+        dashTrailRenderer.emitting = false;
         rb.gravityScale = originalGravity;
         isDashing = false;
         rb.velocity = new Vector2(transform.localScale.x, 0);
         yield return new WaitForSeconds(dashingCooldown);
-        canDash = true;
         isDashingCooldown = false;
 
     }
