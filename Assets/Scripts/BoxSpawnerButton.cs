fb0e92bb3424e70b52a7a7aed6c0752df366d378
RobinOhman <robinohmantejre@gmail.com>
2023-09-22 14:51:54 +0200

    Door Sprite Ändrad
    Gjorde så att man kan sätta max antalet lådor som kan spawnas via en knapp

--- Assets/Scripts/BoxSpawnerButton.cs
+++ Assets/Scripts/BoxSpawnerButton.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class BoxSpawnerButton : MonoBehaviour
 {
     [SerializeField] BoxSpawner boxSpawner;
-
+    [SerializeField] float MaxBoxes = 0f;
+    float BoxesSpawned = 0f;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player")) 
         {
-            
-            boxSpawner.spawnBox = true;
+            if (BoxesSpawned < MaxBoxes) 
+            {
+                boxSpawner.spawnBox = true;
+                BoxesSpawned++;
+            }
+            else
+            {
+                return;
+            }
+           
         }
+        
         
     }
 
--- Assets/Scripts/BoxSpawnerButtonBlue.cs
+++ Assets/Scripts/BoxSpawnerButtonBlue.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 public class BoxSpawnerButtonBlue : MonoBehaviour
 {
     [SerializeField] BoxSpawner boxSpawner;
+    [SerializeField] float MaxBoxes = 0f;
+    float BoxesSpawned = 0f;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player2"))
         {
 
-            boxSpawner.spawnBox = true;
+            if (BoxesSpawned < MaxBoxes)
+            {
+                boxSpawner.spawnBox = true;
+                BoxesSpawned++;
+            }
+            else
+            {
+                return;
+            }
         }
 
     }
